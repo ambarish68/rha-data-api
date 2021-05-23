b2d7f986bf59970a3f3a13f5ed12531f8d96c094
Ambarish Rao <ambarish.r@turvo.com>
2021-05-23 19:51:16 +0530

    RHA-API-5gehg6:
    1. Add CORS enabled for all paths

--- src/main/java/com/rha/dataapi/DataApiApplication.java
+++ src/main/java/com/rha/dataapi/DataApiApplication.java
@@ -2,6 +2,9 @@ package com.rha.dataapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DataApiApplication {
@@ -10,4 +13,13 @@ public class DataApiApplication {
         SpringApplication.run(DataApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("http://localhost:8000");
+            }
+        };
+    }
 }
